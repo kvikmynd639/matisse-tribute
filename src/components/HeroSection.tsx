6d808ce26378c8168104707d999962b8be2f7db7
Sumeja <sumeja.buljugija@gmail.com>
2024-06-27 10:37:03 +0200

    again

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 // src/components/HeroSection.tsx
 
-import { component$, emit } from '@builder.io/qwik';
+import { component$} from '@builder.io/qwik';
 import '../styles/hero.scss';
 
 export default component$(() => {
